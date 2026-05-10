d84fd138282f1209c7c48cfa99711a427fa46676
TamHoang <TamHoang@DESKTOP-TIOBR14>
2026-05-10 20:25:02 +0700

    feat: add FramesLibraryWindow and SmartBatchPlotWindow UIs with custom dark theme styling

---
+++ Reserve/FramesLibraryWindow.xaml.cs
@@ -0,0 +1,47 @@
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TPL
+{
+    public partial class FramesLibraryWindow : Window
+    {
+        public ObservableCollection<FrameItem> Frames { get; set; }
+
+        public FramesLibraryWindow()
+        {
+            InitializeComponent();
+            
+            // Dummy data for preview
+            Frames = new ObservableCollection<FrameItem>
+            {
+                new FrameItem { Name = "A1_TitleBlock", Type = "Block", Size = "841x594", Orientation = "Landscape" },
+                new FrameItem { Name = "A3_TitleBlock", Type = "Block", Size = "420x297", Orientation = "Landscape" },
+                new FrameItem { Name = "A4_Portrait", Type = "Polyline", Size = "210x297", Orientation = "Portrait" }
+            };
+
+            gridFrames.ItemsSource = Frames;
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public class FrameItem
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string Orientation { get; set; }
+    }
+}
